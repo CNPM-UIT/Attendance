8b2ccbc7c6bdc750b3b3ae138d44cd1755b2c2df
Phan Truong Thanh <knightofk9@gmail.com>
2019-12-24 20:53:04 +0700

    Update

---
+++ App_Desktop/App_Desktop/ViewModels/CreateUpdateTestViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App_Desktop.Model.Class;
+
+namespace App_Desktop.ViewModels
+{
+    public class CreateUpdateTestViewModel
+    {
+        private TestItemDetailModel test;
+
+        public CreateUpdateTestViewModel()
+        {
+            test = new TestItemDetailModel(10);
+        }
+        public TestItemDetailModel Test
+        {
+            get => test;
+            set => test = value;
+        }
+    }
+}
