56ebd3bb2003d51bb72c88aa39c50e43d3e7e507
Thành Thật Thà <knightofk9@gmail.com>
2020-01-07 21:22:22 +0700

    Add MýQL

--- WebApp/WebApplication2/Startup.cs
+++ WebApp/WebApplication2/Startup.cs
@@ -66,7 +66,7 @@ namespace WebApplication2
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql(Configuration.GetConnectionString("MySQLConnection")));
 
 
             services.AddAuthorization(options => { });
